ed7892a7f240be556be68a55fa74bc4556e97702
Theo zibell <theo@zibell.se>
2021-11-09 14:56:38 +0100

    klat

--- Mathter_of_the_Universe/Program.cs
+++ Mathter_of_the_Universe/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 
-namespace Mathter_of_the_Universe
+class Program
 {
-    class Program
+
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+
+        if (args.Length > 0) Console.WriteLine(args[args.Length - 1]);
+
+        int X = 0;
+        string input = "";
+        int ii = 0;
+        int poäng = 0;
+
+
+        //första frågan
+        while (ii < 1)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("x * 6 = 42");
+            Console.WriteLine("Vad är x");
+            input = Console.ReadLine();
+
+            //testar om sifra 
+            bool lyckad = int.TryParse(input, out X);
+            if (lyckad == true)
+            {
+                if (X == 7)
+                {
+                    System.Console.WriteLine("Du hade rätt");
+                    poäng++;
+                }
+                else
+                {
+                    System.Console.WriteLine("Du hade fel");
+                    System.Console.WriteLine(X * 6);
+                    if (X < 7){System.Console.WriteLine("Din gisning var för låg");
+                    } else { System.Console.WriteLine("din gisning är för hög"); }
+                }
+                ii++;
+            }
         }
+
+        //avslut 
+        System.Console.WriteLine($"du fick {poäng} poäng");
+        Console.ReadLine();
+        
     }
 }
