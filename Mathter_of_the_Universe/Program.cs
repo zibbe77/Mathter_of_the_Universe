a87653188a982e7c9212e1880a97ac5641ed7f51
Theo zibell <theo@zibell.se>
2021-11-09 14:11:32 +0100

    test

---
+++ Mathter_of_the_Universe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mathter_of_the_Universe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
